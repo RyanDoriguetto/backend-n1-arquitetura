9e15684fa432bd8b005ef5f0d1fd43b54970dc35
Ryan <ryandasilvadoriguetto@gmail.com>
2025-05-09 18:38:37 -0300

    alteracoes 09-05

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+let itens = []; // agora é let (pode mudar)
+
+app.post('/itens', (req, res) => {
+  const item = req.body;
+  item.id = itens.length + 1; // Aqui é item, não novoItem
+  itens.push(item);
+  res.status(201).send('Item criado!');
+});
+
+app.get('/itens', (req, res) => {
+  res.json(itens);
+});
+
+app.put('/itens/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const novoItem = req.body;
+  const index = itens.findIndex(item => item.id === id); // procura pelo ID correto
+  if (index !== -1) {
+    novoItem.id = id; // mantém o mesmo id
+    itens[index] = novoItem;
+    res.send('Item atualizado!');
+  } else {
+    res.status(404).send('Item não encontrado!');
+  }
+});
+
+app.delete('/itens/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = itens.findIndex(item => item.id === id);
+  if (index !== -1) {
+    itens.splice(index, 1);
+    res.send('Item deletado!');
+  } else {
+    res.status(404).send('Item não encontrado!');
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
